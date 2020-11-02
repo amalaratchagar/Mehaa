589db3b53a26f2c33ef73fae695cee5b55bc3c86
ak <amalaratchagar@live.com>
2020-11-03 02:05:48 +0530

    Cleaner version of item module

--- Infra/Respositories/ItemRepositoryAsync.cs
+++ Infra/Respositories/ItemRepositoryAsync.cs
@@ -5,16 +5,24 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Infrastructure.Respositories
 {
     public class ItemRepositoryAsync : GenericRepositoryAsync<Item>, IItemRepositoryAsync
     {
         private readonly DbSet<Item> _items;
+        private readonly MehaaDb _dbContext;
 
         public ItemRepositoryAsync(MehaaDb dbContext) : base(dbContext)
         {
             _items = dbContext.Set<Item>();
+            _dbContext = dbContext;
+        }
+
+        public new async Task<IReadOnlyList<Item>> GetAllAsync()
+        {
+            return await _dbContext.Set<Item>().Include(item => item.Category).ToListAsync();
         }
     }
 }
